cb5d2178a3068b336a2b1288a1010acc25d433c8
Oğuz Kırçiçek <2013010206098@ogrenci.karabuk.edu.tr>
2018-03-12 00:16:24 +0300

    update

--- Python Projelerim/newsdatamysql-beatifulsoup/newsdata.py
+++ Python Projelerim/newsdatamysql-beatifulsoup/newsdata.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
-import pymysql
+import pymysql.cursors
 
 url="https://news.google.com/news/headlines?hl=tr&gl=TR&ned=tr_tr"
 response=requests.get(url)
 
 
 baglanti = pymysql.connect('localhost','root','','newsdata', autocommit=True,charset='utf8')
-baglanti = baglanti.cursor()
+connect = baglanti.cursor()
 
 
 
@@ -19,18 +19,19 @@ basliklar=soup.find_all("a",{"class":"nuEeue hzdq5d ME7ew"})
 kaynaklar=soup.find_all("span",{"class":"IH8C7b Pc0Wt"})
 saatler=soup.find_all("span",{"class":"d5kXP YBZVLb"})
 keyler=soup.find_all("c-wiz",{"class":"HzT8Gd QGRmIf"})
+
 try:
-    with baglanti as cursor:
+    with baglanti.cursor() as cursor:
+            for link,baslik,kaynak,saat,key in zip(linkler,basliklar,kaynaklar,saatler,keyler):
+                url=str(link.get("href").replace("'"," "))
+                title=str(baslik.text.replace("'"," "))
+                source=str(kaynak.text.replace("'"," "))
+                hours=str(saat.text.replace("'"," "))
+                anahtar=str(key.text.replace("'"," "))
+                sorgu = ("INSERT INTO newsdata (newstitle,newsurl,newsource,newstime,newskey) VALUES (%s,%s,%s,%s,%s)")
+                data=title,url,source,hours,anahtar
+                cursor.execute(sorgu,data)
+    baglanti.commit()
 
-        for link,baslik,kaynak,saat,key in zip(linkler,basliklar,kaynaklar,saatler,keyler):
-            url=link.get("href")
-            title=baslik.text
-            source=kaynak.text
-            hours=saat.text
-            anahtar=key.text
-            sorgu = "INSERT INTO newsdata ('newstitle','newsurl','newsource','newstime','newskey') VALUES (%%s,%%s,%%s,%%s,%%s)"
-            cursor.execute(sorgu,(url,title,source,hours,anahtar))
-
-    cursor.commit()
 finally:
     baglanti.close()
